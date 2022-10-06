df7b3b1532b17084bf4a677d7b8428fe9da050df
andersons1lva <andersons1lva@yahoo.com.br>
2022-10-06 18:37:59 -0300

    Create functions (incluirItemNoCarrinho, verCarrinho and fecharCarrinho) in class CarrinhoResource

--- src/main/java/com/anderson/carrinho/resource/CarrinhoResource.java
+++ src/main/java/com/anderson/carrinho/resource/CarrinhoResource.java
@@ -1,4 +1,28 @@
 package com.anderson.carrinho.resource;
 
+import com.anderson.carrinho.model.Carrinho;
+import com.anderson.carrinho.model.Item;
+import com.anderson.carrinho.service.CarrinhoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/ifood-devweek/carrinhos")
+@RequiredArgsConstructor
 public class CarrinhoResource {
+
+    private final CarrinhoService carrinhoService;
+    @PostMapping
+    public Item incluirItemNoCarrinho(@RequestBody ItemDto itemDto){
+
+        return carrinhoService.incluirItemNoCarrinho(itemDto);
+    }
+    @GetMapping ("/{id}")
+    public Carrinho verCarrinho(@PathVariable("id") Long id){
+        return carrinhoService.verCarrinho(id);
+    }
+    @PatchMapping("/fecharCarrinho/{carrinhoId}")
+    public Carrinho fecharCarrinho(@PathVariable("carrinhoId") Long carrinhoId, @RequestParam("formaPagamento") int formaPagamento){
+        return carrinhoService.fecharCarrinho(carrinhoId, formaPagamento);
+    }
 }
