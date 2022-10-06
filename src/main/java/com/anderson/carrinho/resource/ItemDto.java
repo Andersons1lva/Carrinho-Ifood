594bd58d14dec5dc8723cb6b295744b1c084cf2d
andersons1lva <andersons1lva@yahoo.com.br>
2022-10-06 17:29:39 -0300

    Create Class CarrinhoResource and attributes in class ItemDto

--- src/main/java/com/anderson/carrinho/resource/ItemDto.java
+++ src/main/java/com/anderson/carrinho/resource/ItemDto.java
@@ -1,4 +1,19 @@
 package com.anderson.carrinho.resource;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Embeddable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Embeddable
+@Data
 public class ItemDto {
+    private Long produtoId;
+    private int quantidade;
+    private Long carrinhoId;
 }
